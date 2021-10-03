6f727eb0b50a6a39997c2e08289b7a5fcacfbc7b
Shashwat-creator <69109132+Shashwat-creator@users.noreply.github.com>
2021-10-03 18:11:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
 from flask import Flask, render_template, request
+import subprocess
+def create_file(code):
+    with open("eg1.c","w") as target_file:
+        target_file.write(code)
 
+def run_file(file_name):
+    application="c:\\MinGW\\bin\\gcc.exe"
+    with open("output.txt","w+") as output_file:
+        with open("error.txt","w+") as error_file:
+            subprocess.run([application,"eg1.c","-o","eg1.exe"],stdout=output_file,stderr=error_file)
+            output_file.seek(0)
+            error_file.seek(0)
+            output=output_file.read()
+            error=error_file.read()
+    if len(error)>0:return error
+    application="eg1.exe"
+    with open("output.txt","w+") as output_file:
+        with open("error.txt","w+") as error_file:
+            subprocess.run([application,"eg1.c","-o","eg1.exe"],stdout=output_file,stderr=error_file)
+            output_file.seek(0)
+            error_file.seek(0)
+            output=output_file.read()
+            error=error_file.read()
+    if len(output)>0:return output
 app=Flask(__name__)
 
 @app.route('/')
@@ -7,9 +30,14 @@ def home():
  return render_template("index.html")
 
 @app.route('/login',methods = ['POST'])  
-def login():  
+def login(): 
+ print(request,type(request)) 
  uname=request.json
- ans={'outputcode':uname['inputcode']}
+ print(uname,type(uname))
+ code=uname['inputcode']
+ create_file(code)
+ output=run_file("eg1.c")
+ ans={'outputcode':output}
  return ans 
 
 
