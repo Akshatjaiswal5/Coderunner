adff45da076b2e7bf895e1ce1c790f6b94f03429
Shashwat-creator <69109132+Shashwat-creator@users.noreply.github.com>
2021-10-03 18:15:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function run(){
+ 
+  const code=document.getElementById("codebox");
+ 
+  const shell=document.getElementById("shellbox");
+
+  const data = { inputcode: code.value };  
+  
+  fetch('http://localhost:5000/login',{
+   method: 'POST',
+   headers:{ 'Content-Type': 'application/json',},
+   body:JSON.stringify(data),
+  })
+  .then(response => response.json())
+
+
+  .then(response => shell.innerHTML=response.outputcode)
+ }
